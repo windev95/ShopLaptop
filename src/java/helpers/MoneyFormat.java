ab801a4c3075f19c489ffcd4440451577cb2e7d0
Quoc Toan <Mail.Toan95@gmail.com>
2016-12-28 17:19:58 +0700

    Up code

---
+++ src/java/helpers/MoneyFormat.java
@@ -0,0 +1,14 @@
+package helpers;
+import java.text.DecimalFormat;
+public class MoneyFormat {
+    public static String format(int money) {
+        DecimalFormat formatter = new DecimalFormat("###,###,###");            
+        return formatter.format(money) + " VNĐ";
+    }
+    
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Kết quả định dạng: " + format(1000000000));
+    }
+    
+}
