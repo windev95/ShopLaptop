37865e6b4678bfc9e92aa9a68bc6eda17076f0de
Quoc Toan <Mail.Toan95@gmail.com>
2016-12-30 22:05:08 +0700

    Sửa lỗi đăng ký lỗi Font

--- src/java/controller/UserServlet.java
+++ src/java/controller/UserServlet.java
@@ -38,12 +38,15 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
         processRequest(request, response);
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
         String command = request.getParameter("command");
         String url = "";
         Users users = new Users();
